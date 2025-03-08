dac89b7d95980ee55169d59a278c7a2be6387c45
eduardbar <119897290+eduardbar@users.noreply.github.com>
2025-03-08 17:30:00 -0500

    fix: eliminar archivos dist y actualizar .gitignore

--- backend/dist/controllers/analysis.controller.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Request, Response } from 'express';
-export declare const handleAnalyzeProduct: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const getAnalysisHistory: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=analysis.controller.d.ts.map
